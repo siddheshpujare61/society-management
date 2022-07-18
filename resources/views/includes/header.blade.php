1f558084eb66209f37a752ee5cb7a47f8c85240a
ajits11 <sawant.ajit11@gmail.com>
2022-07-18 22:20:59 +0530

    design changes menu added and dropdown in header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,6 +1,11 @@
 <header>
   <div class="logo"><a href="#"><img src="{{ asset('dashboard/images/logo.png') }}" alt=""></a></div>
   <div class="menu-btn"></div>
+  <div class="col-xs-8 col-sm-8 col-md-8 col-lg-3 optionsmain">
+  <select name="global_society" id="global_society" class=" form-control select2 chosen-select">
+    <option value="">Please Select Society</option>
+  </select>  
+</div>
   <div class="user-menu">
       <ul>
         <li><a href="#" title="My Profile">My Profile</a></li>
--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,6 +1,7 @@
 <nav id="sidebar">
     <ul>
         <li><a class="active" href="{{ route('society.index') }}">Society Management</a></li>
+        <li><a class="" href="#">Society Members</a></li>
        <!--  <li><a class="" href="#">My task</a></li>
         <li><a class="" href="#">My Projects</a></li>
         <li><a class="" href="#">My Proposal</a></li> -->
